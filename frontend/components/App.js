c3928821263513b6aafcbefaee9c7c0a6bc5ee09
Elizabeth Doyle <myoddjourney@gmail.com>
2023-09-23 16:46:07 -0400

    started project

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,17 @@
 import React from 'react'
+import TodoList from './TodoList'
+import Form from './Form'
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
   render() {
-    return null
+    return (
+      <div>
+        <h1>App</h1>
+        <Form />
+      <TodoList />
+      </div>
+    )
   }
 }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import Todo from './Todo'
 
 export default class TodoList extends React.Component {
   render() {
-    return null
+    return (
+      <div>
+        <h1>TodoList</h1>
+      <Todo />
+      </div>
+    )
   }
 }
